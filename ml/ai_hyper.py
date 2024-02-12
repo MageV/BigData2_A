7bc589ae06959c96ef10512411393cb491c9e22e
master <morgoth2006@gmail.com>
2024-02-12 17:32:05 +0300

    finish:database operations.Success!!! to-do:AI Model

--- ml/ai_hyper.py
+++ ml/ai_hyper.py
@@ -1,13 +1,13 @@
 param_elastic = {
     'alpha': [0.001, 0.01, 0.1, 0.5, 0.75, 0.79, 1, 1.5, 2, 5],
-    'l1_ratio': [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8],
+    'l1_ratio': [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8]
 }
 
 param_rf = {
-    'n_estimators': range(1, 5),
-    'max_features': ['log2', 'sqrt', ],
+    'n_estimators': range(1, 10),
+    'max_features': ['log2', 'sqrt'],
     'min_samples_split': range(1, 15),
-    'max_depth': range(2, 10)
+    'max_depth': range(2, 20)
 }
 param_lasso = {
     'alpha': [0.001, 0.01, 0.1, 0.5, 0.75, 0.79, 1, 1.5, 2, 5],
@@ -20,7 +20,22 @@ param_hbr = {
     'l2_regularization': [0.001, 0.01, 0.1, 0.2, 0.3, 0.4, 0.5, 0.75, 0.79, 1],
     'max_iter': [200]
 }
-param_svc= {
+param_svc = {
     'kernel': ('linear', 'rbf'),
     'C': [1, 10, 100]
+}
+param_lr = {
+    'penalty': ('l1', 'l2', 'elasticnet'),
+    'solver': ('saga', 'sag', 'liblinear', 'newton-cg', 'newton-cholesky'),
+    'multi_class': ('ovr', 'multinomial'),
+    'class_weight': ['balanced'],
+    'random_state': range(0, 10),
+    'C': (0.1, 0.15, 0.25, 0.35, 0.45, 0.55, 0.65, 0.75, 0.9, 1.0, 1.5, 2.0),
+}
+
+param_dtr = {
+    'max_features': ['log2', 'sqrt'],
+    'min_samples_split': range(1, 20),
+    'max_depth': range(2, 20),
+    'max_leaf_nodes': range(2, 20)
 }
--- ml/ai_model.py
+++ ml/ai_model.py
@@ -7,8 +7,8 @@ import joblib
 import pandas as pd
 from joblib import parallel_backend
 from sklearn import metrics
-from sklearn.ensemble import RandomForestRegressor, ExtraTreesRegressor
-from sklearn.linear_model import Lasso, Ridge, ElasticNet
+from sklearn.ensemble import RandomForestRegressor, ExtraTreesRegressor, HistGradientBoostingRegressor
+from sklearn.linear_model import Lasso, Ridge, ElasticNet, LogisticRegression
 from sklearn.metrics import mean_absolute_percentage_error
 from sklearn.experimental import enable_halving_search_cv
 from sklearn.model_selection import train_test_split, RepeatedKFold, HalvingGridSearchCV
@@ -48,7 +48,7 @@ def ai_learn(mean_over, features=None, scaler=AI_SCALER.AI_NONE):
     df_Y = pre_work_data['workers'].values
     # Расчет моделей и выбор наилучшей
     with parallel_backend("multiprocessing", n_jobs=multiprocessing.cpu_count() - 2):
-        X_train, X_test, Y_train, Y_test = train_test_split(df_X, df_Y, test_size=0.3)
+        X_train, X_test, Y_train, Y_test = train_test_split(df_X, df_Y, test_size=0.15)
         if scaler == AI_SCALER.AI_STD:
             scaler = StandardScaler()
             X_train_scaled = scaler.fit_transform(X_train)
@@ -56,10 +56,9 @@ def ai_learn(mean_over, features=None, scaler=AI_SCALER.AI_NONE):
         elif scaler == AI_SCALER.AI_NONE:
             X_train_scaled = X_train
             X_test_scaled = X_test
-        cv = RepeatedKFold(n_splits=10, n_repeats=3, random_state=1)
         models = [ElasticNet(), Lasso(), Ridge(),
                   RandomForestRegressor(), DecisionTreeRegressor(),
-                  ExtraTreesRegressor(), SVC(gamma='scale')]
+                  ExtraTreesRegressor(), SVC(gamma='scale'), LogisticRegression()]
         last_estimation = 0
         for _ in range(len(models)):
             current_model = models[_]
@@ -69,9 +68,10 @@ def ai_learn(mean_over, features=None, scaler=AI_SCALER.AI_NONE):
                                           f'started learning at:{dt.datetime.now()}', severity=SEVERITY.INFO))
 
             if current_name == 'elasticnet':
-                search = HalvingGridSearchCV(current_model, param_elastic, cv=cv, verbose=3)
+                search = HalvingGridSearchCV(current_model, param_elastic, cv=5,
+                                             scoring='r2', verbose=3, factor=2, n_jobs=-1, min_resources=20)
                 current_model = search
-            elif current_name == 'randomforestregressor' or current_name == 'extratreesregressor' or current_name == 'decisontreeregressor':
+            elif current_name == 'randomforestregressor' or current_name == 'extratreesregressor':
                 search = HalvingGridSearchCV(current_model, param_rf, cv=5,
                                              scoring='r2', verbose=3, factor=2, n_jobs=-1, min_resources=20)
                 current_model = search
@@ -91,12 +91,21 @@ def ai_learn(mean_over, features=None, scaler=AI_SCALER.AI_NONE):
                 search = HalvingGridSearchCV(current_model, param_svc, cv=5,
                                              scoring='r2', verbose=3, factor=2, n_jobs=-1, min_resources=20)
                 current_model = search
+            elif current_name == 'logisticregression':
+                search = HalvingGridSearchCV(current_model, param_lr, cv=5,
+                                             scoring='r2', verbose=3, factor=2, n_jobs=-1, min_resources=20)
+                current_model = search
+            elif current_name == 'decisiontreeregressor':
+                search = HalvingGridSearchCV(current_model, param_dtr, cv=5,
+                                            scoring='r2', verbose=3, factor=2, n_jobs=-1, min_resources=20)
+                current_model = search
 
             current_model.fit(X_train_scaled, Y_train)
             result = current_model.predict(X_test_scaled)
             estimation_mae = mean_absolute_percentage_error(Y_test, result)
             estimation_r2 = metrics.r2_score(Y_test, result)
             models_results.update({current_model.__repr__().split('(')[0].lower(): (estimation_mae, estimation_r2)})
+            asyncio.run(write_log(message=f"Model:{str(current_model.best_estimator_).split('(')[0]}", severity=SEVERITY.INFO))
             asyncio.run(write_log(message=f"R2:(good ~1,bad ~0) ,"
                                           f" {estimation_r2}", severity=SEVERITY.INFO))
             asyncio.run(write_log(message=f"MAPE(%): ,"
@@ -111,7 +120,7 @@ def ai_learn(mean_over, features=None, scaler=AI_SCALER.AI_NONE):
             result = None
             gc.collect()
     asyncio.run(write_log(message=print(models_results), severity=SEVERITY.INFO))
-    name = best_model.__repr__().split('(')[0].lower()
+    name = str(best_model.best_estimator_).split('(')[0]
     try:
         joblib.dump(best_model, f'{MODEL_STORE}{name}_aimodel.gzip', compress=3)
     except Exception as ex:
